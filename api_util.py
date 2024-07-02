43b3200a580a13cbac64143cdb8df9eeceada6bf
Christopher Slawter <slawterc@gmail.com>
2024-07-02 11:15:31 -0400

    Adding a utility file and testing strategies.

--- api_util.py
+++ api_util.py
@@ -1,18 +1,19 @@
 import requests
-import json
 from ai_util import estimate_sentiment
 
 class REST():
-    def __init__(self, CREDS:dict):
-        self.CREDS = CREDS
-
-    def get_headlines(self, start:str, end:str, symbol:str):
-        url = (f"https://data.alpaca.markets/v1beta1/news?start={start}&end={end}&sort=desc&symbols={symbol}&include_content=false")
-        headers = {
+    def __init__(self, ALPACA_CREDS:dict):
+        self.creds = ALPACA_CREDS
+        self.headers = {
             "accept": "application/json",
-            "APCA-API-KEY-ID": self.CREDS['API_KEY'],
-            "APCA-API-SECRET-KEY": self.CREDS['API_SECRET']
+            "APCA-API-KEY-ID": self.creds['API_KEY'],
+            "APCA-API-SECRET-KEY": self.creds['API_SECRET']
         }
+        self.base_url = "https://data.alpaca.markets/v1beta1/"
+
+    def get_headlines(self, start:str, end:str, symbol:str) -> list:
+        url = (f"{self.base_url}news?start={start}&end={end}&sort=desc&symbols={symbol}&include_content=false")
+        headers = self.headers
         response = requests.get(url, headers=headers)
         res_json = response.json()
 
@@ -22,4 +23,17 @@ class REST():
             headlines.append(n["headline"])
 
         return headlines
-
+    
+    def get_top_movers(self, limit:int) -> dict:
+        url = (f"{self.base_url}screener/stocks/movers?top={limit}")
+        headers = self.headers
+        response = requests.get(url, headers=headers)
+        movers = response.json()
+        return movers
+    
+    def get_most_active(self, limit:int) -> dict:
+        url = (f"{self.base_url}screener/stocks/most-actives?by=volume&top={limit}")
+        headers = self.headers
+        response = requests.get(url, headers=headers)
+        most_active = response.json()
+        return most_active
--- trading_bot.py
+++ trading_bot.py
@@ -6,9 +6,10 @@ from datetime import datetime
 from timedelta import Timedelta
 from ai_util import estimate_sentiment
 from api_util import REST
+from strategy_util import get_symbol_ticker_df, get_MACD_with_signals
 import json
 
-creds = open('CREDS.json')
+creds = open('ALPACA_CREDS.json')
 ALPACA_CREDS = json.load(creds)
 
 ALPACA ={
@@ -25,10 +26,10 @@ cash_at_risk = .5
 class MLTrader(Strategy):
     def initialize(self, symbol:str=symbol, cash_at_risk:float=cash_at_risk):
         self.symbol = symbol
-        self.sleeptime = "24H"
+        self.sleeptime = "5M"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
-        self.api_util = REST(CREDS=ALPACA)
+        self.api_util = REST(ALPACA_CREDS=ALPACA)
 
     def postion_sizing(self):
         cash = self.get_cash()
@@ -49,41 +50,59 @@ class MLTrader(Strategy):
         probability, sentiment = estimate_sentiment(news)
         return probability, sentiment
     
+    def get_macd_signals(self):
+        end, start = self.get_dates()
+        df = get_symbol_ticker_df(symbol=self.symbol, start_date=start, end_date=end)
+        signals_df = get_MACD_with_signals(df)
+        if signals_df.empty:
+            return 'Hold'
+        else:
+            lastrow=signals_df.iloc[-1]
+            return lastrow['order']
+
+    
     def on_trading_iteration(self):
         cash, last_price, quantity =  self.postion_sizing()
         probability, sentiment = self.get_sentiment()
+        order_type = self.get_macd_signals()
 
         if cash > last_price:
-            if sentiment == "positive" and probability > .999: 
-                if self.last_trade == "sell": 
-                    self.sell_all() 
-                order = self.create_order(
-                    self.symbol, 
-                    quantity, 
-                    "buy", 
-                    type="bracket", 
-                    take_profit_price=last_price*1.20, 
-                    stop_loss_price=last_price*.95
-                )
-                self.submit_order(order) 
-                self.last_trade = "buy"
-            elif sentiment == "negative" and probability > .999: 
+            if order_type == 'Buy':
+                if sentiment == "positive" and probability > .999: 
+                    if self.last_trade == "sell": 
+                        self.sell_all() 
+                    order = self.create_order(
+                        self.symbol, 
+                        quantity, 
+                        "buy", 
+                        type="market"
+                        # type="bracket", 
+                        # take_profit_price=last_price*1.25, 
+                        # stop_loss_price=last_price*.95
+                    )
+                    self.submit_order(order) 
+                    self.last_trade = "buy"
+                    order_type == ""
+            elif order_type == 'Sell' and self.last_trade != None:
+            #sentiment == "negative" and probability > .98: 
                 if self.last_trade == "buy": 
                     self.sell_all() 
                 order = self.create_order(
                     self.symbol, 
                     quantity, 
                     "sell", 
-                    type="bracket", 
-                    take_profit_price=last_price*.8, 
-                    stop_loss_price=last_price*1.05
+                    type="market"
+                    # type="bracket", 
+                    # take_profit_price=last_price*.8, 
+                    # stop_loss_price=last_price*1.05
                 )
                 self.submit_order(order) 
                 self.last_trade = "sell"
+                order_type = ""
 
 
-start_date = datetime(2024,6,1)
-end_date = datetime(2024,6,29)
+start_date = datetime(2024,6,20)
+end_date = datetime(2024,6,30)
 broker = Alpaca(ALPACA)
 strategy = MLTrader(name="mlstrat", broker=broker, parameters={"symbol":symbol, "cash_at_risk":cash_at_risk})
 
